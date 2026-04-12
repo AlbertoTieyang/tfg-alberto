b0f57a8d75427b42a775b5a058f888576d848cde
AlbertoTieyang <alberto.gomez22@educa.madrid.org>
2026-04-12 16:25:56 +0200

    Creación de las rutas (no terminadas), inicio de fortify y header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,31 @@
-<header>
-    <ul>
-        <li><a>Nosotros</a></li>
-        <li><a>Inicio</a></li>
-        <li><a>Carta</a></li>
-    </ul>
+<header class="bg-light border-bottom">
+  <div class="container">
+    <div class="d-flex align-items-center justify-content-between py-2">
+      <a href="{{ $leftLink ?? '#' }}" class="text-decoration-none fw-bold">
+        {{ $leftText ?? 'Mi Link' }}
+      </a>
+      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#menuNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <nav class="collapse navbar-collapse justify-content-center" id="menuNav">
+        <ul class="navbar-nav flex-lg-row gap-lg-4 text-center">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ $inicio ?? '#' }}">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ $nosotros ?? '#' }}">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ $reservas ?? '#' }}">Reservas</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="d-none d-lg-block">
+        <img src="{{ $logo ?? 'https://via.placeholder.com/50' }}" 
+             alt="Logo" 
+             class="img-fluid" 
+             style="max-height:50px;">
+      </div>
+    </div>
+  </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Ruta cuando entras a la página
+Route::get("/", [PageController::class, "index"])->name("index");
+
+//Ruta para página nosotros
+Route::get("/nosotros", [PageController::class, "nosotros"])->name("nosotros");
+
+//Ruta para carta
+
+
+//Ruta para hacer las reservas
+
+
+//Ruta para login 
+
+//Ruta para registro 
