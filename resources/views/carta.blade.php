9fb70c8cd8038297ebf1564622c996eddf5ea83e
AlbertoT <alberto.gomez22@educa.madrid.org>
2026-04-29 18:43:53 +0200

    empiezado la parte de búsqueda en la carta

--- resources/views/carta.blade.php
+++ resources/views/carta.blade.php
@@ -14,12 +14,27 @@
     @endauth
     <div class="container">
         <div class="row">
-            <div class="col">
-                @foreach ($platos as $plato)
-                <p>{{ $plato->nombre }}</p>
-            
-                @endforeach
+            <div class="col mb-3">
+                <form action="{{ route('plato.search') }}"></form>
+                <label for="nombre">Nombre</label>
+                <input type="text">
             </div>
+            <div></div>
+            @foreach ($platos as $plato)
+            <div class="col-3 mb-3">
+                <div class="card" style="width: 18rem; min-height: 200px;">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $plato->nombre }}</h5>
+                    <h6 class="card-subtitle">{{ $plato->precio }}€</h6>
+                    <p class="card-text text-justify">{{ $plato->descripcion }}</p>
+                    @auth
+                    <a href="{{ route('plato.create') }}" class="btn btn-primary">Editar</a>
+                    @endauth
+                </div>
+                </div>  
+            </div>
+                @endforeach
         </div>    
     </div>
 </body>
